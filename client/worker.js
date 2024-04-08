2a32164586d128c8a16a85e9defeff6868e5f774
Alexander Polyanskiy <43495209+Alexsandr52@users.noreply.github.com>
2024-04-08 16:52:49 +0300

    Update worker.js

--- client/worker.js
+++ client/worker.js
@@ -1,42 +1,42 @@
-// const CACHE_NAME = 'node-ex-push-cache-v1';
-// const urlsToCache = [
-//   '/index.html',
-//   '/client.js',
-// ];
+const CACHE_NAME = 'node-ex-push-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/client.js',
+];
 
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request)
-//           .then(function(response) {
-//             if (!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
-//             var responseToCache = response.clone();
-//             caches.open(CACHE_NAME)
-//               .then(function(cache) {
-//                 cache.put(event.request, responseToCache);
-//               });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(function(response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-//             return response;
-//           });
-//       })
-//   );
-// });
+            return response;
+          });
+      })
+  );
+});
 
 self.addEventListener('push', e => {
     const data = e.data.json();
